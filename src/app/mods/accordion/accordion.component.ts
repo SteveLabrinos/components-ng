a9fafaf4b946937cc23bd41ab82e807797034f87
SteveLabrinos <slabrinos9214@gmail.com>
2021-10-18 21:47:38 +0300

    add accordion component to the mods module

---
+++ src/app/mods/accordion/accordion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.css'],
+})
+export class AccordionComponent implements OnInit {
+  @Input() items: { title: string; content: string }[] = [];
+  openedItemIndex = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
